acf5e0875859d658a3e1bc1d5db575acc1959c77
OrdimixDj <56314042+OrdimixDj@users.noreply.github.com>
2025-12-08 23:54:17 +0100

    ex10.4_part10_finished

---
+++ src/components/AppBar.jsx
@@ -0,0 +1,7 @@
+import AppBarTab from "./AppBarTab";
+
+const AppBar = () => {
+  return <AppBarTab text="Repository" />;
+};
+
+export default AppBar;
---
+++ src/components/AppBarTab.jsx
@@ -0,0 +1,31 @@
+import { Text, Pressable, View, StyleSheet } from "react-native";
+import Constants from "expo-constants";
+import theme from "../theme";
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: Constants.statusBarHeight,
+    paddingLeft: 15,
+    paddingTop: Constants.statusBarHeight,
+    paddingBottom: 25,
+    backgroundColor: theme.colors.backgroundColor,
+  },
+  text: {
+    color: theme.colors.menuText,
+    fontWeight: theme.fontWeights.bold,
+  },
+});
+
+const AppBarTab = ({ text }) => {
+  return (
+    <View style={styles.container}>
+      {
+        <Pressable>
+          <Text style={styles.text}>{text}</Text>
+        </Pressable>
+      }
+    </View>
+  );
+};
+
+export default AppBarTab;
